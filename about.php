53761bc6096155068071087515de4f1a6d712434
Luke Lee <durdenmisc@gmail.com>
2012-01-19 22:09:47 -0600

    Make about page more concise

--- about.php
+++ about.php
@@ -1,67 +1,69 @@
-<?php 
-	require('inc/uiHdr.php'); 
+<?php
+    require('inc/uiHdr.php');
 ?>
 
-<h3>About Me</h3>
-
-<p>
-	I received a Bachelor of Science degree in Computer Science with a minor in 
-	Mathematics from <a href="http://www.ttu.edu"> Texas Tech University</a> in 
-	May of 2007.
-</p>
-
-<p>
-    I am currently a Software Engineer at <a href="http://www.texmemsys.com">Texas Memory Systems</a>.
+<div class="about">
+    <p><span class="intro"> I <strong>work</strong></span>
+        as a Software Engineer at <a href="http://www.blueback-reservoir.com">Blueback Reservoir</a>.</p>
+    <p> <span class="intro"> I <strong>graduated</strong></span>
+            from <a href="http://www.ttu.edu"> Texas Tech University</a>
+            with a degree in Computer Science.
+    </p>
+</div>
+<!--
+        as a Software Engineer at <a href="http://www.texmemsys.com">Texas 
+        Memory Systems</a>.
     I have been with the company since May 2007.  I am primarily responsible
-    for our web-based regression testing software as well as low-level device 
+    for our web-based regression testing software as well as low-level device
     driver code.
 </p>
 
 <p>
-	Prior to working at TMS, I was employed at 
-	<a href="http://www.ctsinet.com">Computer Transition Services Inc.</a>
-	in Lubbock, TX for almost 4 years.  I was 
-	a Computer Technician with CTSI until around February of 2005.
+    Prior to working at TMS, I was employed at
+    <a href="http://www.ctsinet.com">Computer Transition Services Inc.</a>
+    in Lubbock, TX for almost 4 years.  I was
+    a Computer Technician with CTSI until around February of 2005.
 </p>
 <p>
-    As of February 2005, I became mainly focused on web development during my 
-    time at CTSI.  I had the privilege of working with two other web developers 
-    during my 3 years of develoment with CTSI.  
+    As of February 2005, I became mainly focused on web development during my
+    time at CTSI.  I had the privilege of working with two other web developers
+    during my 3 years of develoment with CTSI.
 </p>
 <p>
-	I learned a lot about programming in general as well as
-    many benefits of agile software development and teamwork.  I really enjoyed 
+    I learned a lot about programming in general as well as
+    many benefits of agile software development and teamwork.  I really enjoyed
     my time at CTSI and still enjoy web development as one of my hobbies.
 </p>
 
-	The majority of my work at CTSI was developed using the following:
-	<ul>
-		<li>
-			classic <a href="http://en.wikipedia.org/wiki/Active_Server_Pages" title="WikiPedia Link">ASP</a>
-		</li>
-		<li>
-			SQL Server <a href="http://msdn2.microsoft.com/es-es/library/ms131287.aspx" title="MSDN">Stored Procedures</a>
-		</li>
-		<li>
-			<a href="http://en.wikipedia.org/wiki/HTML" title="WikiPedia Link">HTML</a>
-		</li>
-		<li>
-			<a href="http://www.w3.org/Style/CSS/" title="W3C">CSS</a>
-		</li>
-		<li>
-			<a href="http://www.en.wikipedia.com/wiki/JavaScript" title="WikiPedia Link">JavaScript</a>
-		</li>
-		<li>
-			<a href="http://www.en.wikipedia.com/wiki/JScript" title="WikiPedia Link">JScript</a>
-		</li>		
-	</ul>
+    The majority of my work at CTSI was developed using the following:
+    <ul>
+        <li>
+            classic <a href="http://en.wikipedia.org/wiki/Active_Server_Pages" title="WikiPedia Link">ASP</a>
+        </li>
+        <li>
+            SQL Server <a href="http://msdn2.microsoft.com/es-es/library/ms131287.aspx" title="MSDN">Stored Procedures</a>
+        </li>
+        <li>
+        <a href="http://en.wikipedia.org/wiki/HTML" title="WikiPedia Link">HTML</a>
+        </li>
+       <li>
+            <a href="http://www.w3.org/Style/CSS/" title="W3C">CSS</a>
+        </li>
+        <li>
+        <a href="http://www.en.wikipedia.com/wiki/JavaScript" title="WikiPedia Link">JavaScript</a>
+        </li>
+        <li>
+            <a href="http://www.en.wikipedia.com/wiki/JScript" title="WikiPedia Link">JScript</a>
+        </li>
+    </ul>
 <p>
-	For my personal work on the web I use the above technologies and the "Open Source Help".
+    For my personal work on the web I use the above technologies and the "Open Source Help".
 </p>
 <p>
-	For more information on my experience with various technolgies, etc. please see my resume.
+    For more information on my experience with various technolgies, etc. please see my resume.
 </p>
+--->
 
-<?php 
-	require('inc/uiFtr.php');
+<?php
+    require('inc/uiFtr.php');
 ?>
