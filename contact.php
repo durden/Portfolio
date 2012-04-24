1950f51a23a49c9312c23ae09cb4661351202f20
Luke Lee <durdenmisc@gmail.com>
2012-04-24 08:28:58 -0500

    Update email address

--- contact.php
+++ contact.php
@@ -3,5 +3,5 @@
 	$sidebar = 0; 
 ?>
 <h3>Contact</h3>
-<p>Please send information to luke (at) lukelee (dot) net.</p>
+<p>Please send information to charles (dot) luke (dot) (lee) (at) gmail (dot) com</p>
 <?php require('inc/uiFtr.php');?>
